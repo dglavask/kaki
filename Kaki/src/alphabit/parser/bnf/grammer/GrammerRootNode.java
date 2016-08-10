16eb4f88c9524227e4ccada03263a224e358d1df
doox <doox@kucniljubimac.projectalphabit.com>
2016-08-10 22:30:57 +0200

    Added the grammer root node.

---
+++ Kaki/src/alphabit/parser/bnf/grammer/GrammerRootNode.java
@@ -0,0 +1,26 @@
+package alphabit.parser.bnf.grammer;
+
+import alphabit.parser.bnf.GrammerNodeVisitor;
+
+public class GrammerRootNode implements GrammerNode{
+	private String identifier;	
+
+	public GrammerRootNode(String name) {
+	    this.identifier = name;	    
+	  }
+
+	public int getType() {
+		//only for the case of case non sensitive grammer
+		return GrammerNode.GENERIC;
+	}	
+
+	public String getValue() {
+		// removing the <> from the non terminal
+		return null;
+	}
+
+	public void accept(GrammerNodeVisitor visitor) {
+		// TODO Auto-generated method stub
+		visitor.visit(this);
+	}
+}
